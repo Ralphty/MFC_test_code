fd25308c945d8c255a0ef0ca17b0205439a19e37
Ralphty <Ralphance@163.com>
2020-04-27 11:10:48 +0800

    complete UI for MFC program.

--- MFC_test/Resource.h
+++ MFC_test/Resource.h
@@ -2,20 +2,47 @@
 // Microsoft Visual C++ generated include file.
 // Used by MFCtest.rc
 //
-#define IDR_MAINFRAME					128
-#define IDM_ABOUTBOX					0x0010
-#define IDD_ABOUTBOX					100
-#define IDS_ABOUTBOX					101
-#define IDD_MFC_TEST_DIALOG				102
+#define IDM_ABOUTBOX                    0x0010
+#define IDD_ABOUTBOX                    100
+#define IDS_ABOUTBOX                    101
+#define IDD_MFC_TEST_DIALOG             102
+#define IDR_MAINFRAME                   128
+#define IDC_COMBO_NUMBER                1000
+#define IDC_EDIT_LONGITUDE_1            1001
+#define IDC_COMBO_COLOR                 1002
+#define IDC_COMBO_INFORMATION_SOURCE    1003
+#define IDC_COMBO_TYPE_1                1004
+#define IDC_EDIT_CONFIDENCE             1005
+#define IDC_EDIT_LATITUDE_1             1006
+#define IDC_EDIT_SPEED_1                1007
+#define IDC_EDIT_X_1                    1008
+#define IDC_EDIT_COURSE_ANGLE_1         1009
+#define IDC_EDIT_Y_1                    1010
+#define IDC_EDIT_Z_1                    1011
+#define IDC_EDIT_LONGITUDE_2            1012
+#define IDC_COMBO_TYPE_2                1013
+#define IDC_EDIT_LATITUDE_2             1014
+#define IDC_EDIT_SPEED_2                1015
+#define IDC_EDIT_X_2                    1016
+#define IDC_EDIT_COURSE_ANGLE_2         1017
+#define IDC_EDIT_Y_2                    1018
+#define IDC_EDIT_Z_2                    1019
+#define IDC_EDIT_LONGITUDE_3            1020
+#define IDC_COMBO_TYPE_3                1021
+#define IDC_EDIT_LATITUDE_3             1022
+#define IDC_EDIT_SPEED_3                1023
+#define IDC_EDIT_X_3                    1024
+#define IDC_EDIT_COURSE_ANGLE_3         1025
+#define IDC_EDIT_Y_3                    1026
+#define IDC_EDIT_Z_3                    1027
 
 // Next default values for new objects
-//
+// 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-
-#define _APS_NEXT_RESOURCE_VALUE	129
-#define _APS_NEXT_CONTROL_VALUE		1000
-#define _APS_NEXT_SYMED_VALUE		101
-#define _APS_NEXT_COMMAND_VALUE		32771
+#define _APS_NEXT_RESOURCE_VALUE        129
+#define _APS_NEXT_COMMAND_VALUE         32771
+#define _APS_NEXT_CONTROL_VALUE         1002
+#define _APS_NEXT_SYMED_VALUE           101
 #endif
 #endif
